d55e7e50c91a4fcd3aaeba489afd149872df81b5
FatemiSolution <shabbir.high@gmail.com>
2023-10-03 12:29:55 +0500

    initializing .env and Ids from appwrite

---
+++ src/configue/configue.js
@@ -0,0 +1,7 @@
+const configue={
+    appwrite_url:String(import.meta.env.VITE_APP_APPWRITE_URL),
+    project_id:String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+    bucket_id:String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+   database_id:String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
+   collection_id:String(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
+}
